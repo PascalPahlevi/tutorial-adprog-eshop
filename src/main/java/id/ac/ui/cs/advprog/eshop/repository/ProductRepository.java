4803ed64dd4360c631004055f4b9ce519a968900
Pascal Pahlevi <PascalPahlevi@gmail.com>
2024-02-14 15:54:56 +0700

    updated packages

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -19,30 +19,4 @@ public class ProductRepository {
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
-
-    public Product findById(String productId) {
-        for (Product product : productData) {
-            if (product.getProductId().equals(productId)) {
-                return product;
-            }
-        }
-        return null;
-    }
-
-    public Product update(Product updatedProduct) {
-        for (int i = 0; i < productData.size(); i++) {
-            Product existingProduct = productData.get(i);
-            if (existingProduct.getProductId().equals(updatedProduct.getProductId())) {
-                existingProduct.setProductName(updatedProduct.getProductName());
-                existingProduct.setProductQuantity(updatedProduct.getProductQuantity());
-                return updatedProduct;
-            }
-        }
-        return null;
-    }
-
-    public void delete(String productId) {
-        productData.removeIf(product -> product.getProductId().equals(productId));
-    }
-
 }
