08d9af48ad67411f9968b976f191136b21c53df5
Pascal Pahlevi <PascalPahlevi@gmail.com>
2024-02-23 13:37:28 +0700

    Applied SRP to Car Controller

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/EditCarController.java
@@ -0,0 +1,30 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/car")
+public class EditCarController {
+    @Autowired
+    private CarServiceImpl carService;
+
+    @GetMapping("/editCar/{carId}")
+    public String editCarPage(@PathVariable String carId, Model model) {
+        Car car = carService.findById(carId);
+        model.addAttribute("car", car);
+        return "editCar";
+    }
+
+    @PostMapping("/editCar")
+    public String editCarPost(@ModelAttribute Car car, Model model) {
+        System.out.println(car.getCarId());
+        carService.update(car.getCarId(), car);
+
+        return "redirect:listCar";
+    }
+}
