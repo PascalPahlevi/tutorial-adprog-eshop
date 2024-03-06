67c4aacff47c06e8af402203c6dfd0933d385762
Pascal Pahlevi <PascalPahlevi@gmail.com>
2024-03-06 20:21:01 +0700

    [REFACTOR] Apply PaymentStatus enum check to Payment model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -30,20 +31,20 @@ public class PaymentTest {
     @Test
     void testCreateVoucherSuccessStatus() {
         Payment payment = new Payment("2c61b6d0-5619-4f81-9254-a2390fc275f7", "voucherCode", paymentData, "SUCCESS");
-        assertEquals("SUCCESS", payment.getStatus());
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreateVoucherRejectedStatus() {
         Payment payment = new Payment("2c61b6d0-5619-4f81-9254-a2390fc275f7", "voucherCode", paymentData, "REJECTED");
-        assertEquals("REJECTED", payment.getStatus());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
     void testSetStatusToRejected() {
         Payment payment = new Payment("2c61b6d0-5619-4f81-9254-a2390fc275f7", "voucherCode", paymentData);
         payment.setStatus("REJECTED");
-        assertEquals("REJECTED", payment.getStatus());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 
     @Test
