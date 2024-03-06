5aef76c4c722d220ede9f0432da71fcd0fcf206c
Pascal Pahlevi <PascalPahlevi@gmail.com>
2024-03-06 21:50:19 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -5,16 +5,37 @@ import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class PaymentServiceImpl implements PaymentService{
     @Autowired
     private PaymentRepository paymentRepository;
     @Override
-    public Payment createPayment(Payment payment) {return null;}
+    public Payment createPayment(Payment payment) {
+        if (paymentRepository.findById(payment.getId()) == null) {
+            paymentRepository.save(payment);
+            return payment;
+        }
+        return null;
+    }
     @Override
-    public Payment updateStatus(String paymentId, String status) {return null;}
+    public Payment updateStatus(String paymentId, String status) {
+        Payment payment = paymentRepository.findById(paymentId);
+        if (payment != null) {
+            Payment newPayment = new Payment(payment.getId(), payment.getMethod(),
+                    payment.getPaymentData(), status);
+            paymentRepository.save(newPayment);
+            return newPayment;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
     @Override
-    public List<Payment> findAll() {return null;}
+    public List<Payment> findAll() {
+        return paymentRepository.findAll();
+    }
     @Override
-    public Payment findById(String paymentId) {return null;}
+    public Payment findById(String paymentId) {
+        return paymentRepository.findById(paymentId);
+    }
 }
